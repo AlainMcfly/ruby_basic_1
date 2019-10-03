c842256e3dd8022f26577eb0883b841619b0e75e
Florent <florent.sauvetage@gmail.com>
2019-10-03 17:36:02 +0200

    2.2.2 full pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -4,23 +4,24 @@ def half_pyramids
     
     puts "Dis un nombre entre 0 et 25"
     print "> "
-    p = gets.chomp.to_i
+    p = gets.chomp.to_i #donc p=0 à 25
     puts "voici la pyramide :"
 
-    n=1
+    n=1 #n est une variable avec une valeur 1
 
-while n <= p
-  m= p-n
-    if (n == 1)
-        print (" " * m)
+while n <= p #pendant que la variable n est plus petit ou égale à p quand p est égale au nombre rentré.
+  m= p-n #m est une variable qui a une valeur de p=(0 à 25) moins n=(1)
+    if (n == 1)#si n égal à 1 alors
+        print (" " * (m/2))#résultat= ("espace" *fois)
         puts ("#" * n)
     else
-        print (" " * m)
+        print (" " * (m/2))
         puts ("#" * n)
     end
-    n += 1
+    n += 2
 end
 
 end
+
 
 half_pyramids
