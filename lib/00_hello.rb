e62b424d4e74491e43ef6b34d34548b0a29c9ec6
sabrille <sabrille@free.fr>
2019-10-03 12:22:02 +0200

    2.1.2 Coucou Toi

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,5 +1,11 @@
+# 2.1.1 Dis Bonjour
+
 def say_hello
-   puts "Bonjour !"
+    puts "Quel est ton nom ?"
+    print "->"
+    first_name = gets.chomp
+   puts "Bonjour ! #{first_name}"
+    return first_name
 end
 
 say_hello
