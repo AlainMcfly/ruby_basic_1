28ea4edf711668ea50e98c79632c14b7aa5cfa17
sabrille <sabrille@free.fr>
2019-10-03 13:38:44 +0200

    2.1.3 Le Combo

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,11 +1,26 @@
-# 2.1.1 Dis Bonjour
+# 2.1.1 Dis bonjour 
+    #Code une méthode say_hello qui va dire bonjour quand on l'exécute.
 
-def say_hello
-    puts "Quel est ton nom ?"
+# 2.1.2 Coucou toi
+    #Modifie la méthode pour qu'elle prenne désormais en entrée une variable first_name et que ton script affiche "Bonjour, first_name !"
+
+# 2.1.3 Le combo
+    #Maintenant rajoute une méthode ask_first_name qui demande à l'utilisateur son prénom et retourne le résultat avec un return. Combine-la avec ta say_hello(first_name) pour avoir un programme qui demande à l'utilisateur son prénom et lui dit bonjour.
+
+def ask_first_name
+    puts "Quel est ton prénom ?"
     print "->"
     first_name = gets.chomp
-   puts "Bonjour ! #{first_name}"
     return first_name
 end
 
-say_hello
+def say_hello(first_name) # Pour récupérer la variable first_name de ask_first_name, On passe la variable first_name en argument () de la methode say_hello, ce qui signifie que l'on pourra utiliser cette variable dans la méthode
+    puts "Bonjour #{first_name}"
+end
+
+def perform # Execution des précédentes méthodes
+    final_name = ask_first_name # On créé la variable final_name qui va récupérer le return de la methode ask_first_name pour pouvoir ensuite l'utiliser dans l'argument de la methode say_hello(final_name)
+    say_hello(final_name)
+end
+
+perform
