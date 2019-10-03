7976386e3445018fa54e7d53a749bdef260c0289
sabrille <sabrille@free.fr>
2019-10-03 13:56:52 +0200

    2.2.1 Moitié de pyramide

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,26 @@
+#2.2.1 Moitié de pyramide
+
+def half_pyramids
+    
+    puts "Dis un nombre entre 0 et 25"
+    print "> "
+    p = gets.chomp.to_i
+    puts "voici la pyramide :"
+
+    n=1
+
+while n <= p
+  m= p-n
+    if (n == 1)
+        print (" " * m)
+        puts ("#" * n)
+    else
+        print (" " * m)
+        puts ("#" * n)
+    end
+    n += 1
+end
+
+end
+
+half_pyramids
